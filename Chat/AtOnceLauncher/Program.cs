f7b24be050278bba771ad7be291564680595c94a
wlsvy <hubjpkim@gmail.com>
2020-12-24 01:15:12 +0900

    At Once Launcher 진입점 코드 작업

--- Chat/AtOnceLauncher/Program.cs
+++ Chat/AtOnceLauncher/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.IO;
+using System.Diagnostics;
 
 namespace AtOnceLauncher
 {
     class Program
     {
+        public const string ROOT_DIRECTORY_NAME = ".NET-TCP-Chat-Server-Study";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(
+                @"===============================
+At Once Launcher
+===============================");
+
+            var directory = Directory.GetParent(Directory.GetCurrentDirectory());
+            while (directory != null &&
+                directory.Name != ROOT_DIRECTORY_NAME)
+            {
+                directory = directory.Parent;
+            }
+
+            if(directory.Name != ROOT_DIRECTORY_NAME)
+            {
+                Console.WriteLine("프로젝트 루트 폴더를 찾지 못했습니다. 프로그램을 종료합니다.");
+                return;
+            }
+
+            var serverProjectPath = Path.Combine(directory.FullName, "Chat", "Server");
+            var clientProjectPath = Path.Combine(directory.FullName, "Chat", "Client");
+
+            var serverExecutable = Directory.GetFiles(serverProjectPath, "Server.exe", SearchOption.AllDirectories);
+            var clientExecutable = Directory.GetFiles(clientProjectPath, "Client.exe", SearchOption.AllDirectories);
+
+            var serverProcess = Process.Start(serverExecutable[0]);
+
+            while (true) ;
         }
     }
 }
--- Chat/Client/Program.cs
+++ Chat/Client/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 
-namespace Chat
+namespace Client
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            Console.WriteLine("Hello Client!");
         }
     }
 }
