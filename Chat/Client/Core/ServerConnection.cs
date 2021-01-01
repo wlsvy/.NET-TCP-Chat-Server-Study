0133bfe47b9e211ee68540ab5528181b04cc6b7c
wlsvy <hubjpkim@gmail.com>
2021-01-01 15:14:36 +0900

    클라 tcp 연결 기능 작업 중

--- Chat/Client/Core/ServerConnection.cs
+++ Chat/Client/Core/ServerConnection.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Client.Core
 {
@@ -10,5 +9,80 @@ namespace Client.Core
     {
         public readonly long ConnectionId;
 
+        public readonly IPAddress ServerIp;
+        public readonly EndPoint RemoteEndPoint;
+        public readonly Socket ConnectSocket;
+
+        public ServerConnection(long id, IPAddress serverIp, EndPoint remoteEndPoint, Socket socket)
+        {
+            ConnectionId = id;
+            ServerIp = serverIp;
+            RemoteEndPoint = remoteEndPoint;
+            ConnectSocket = socket;
+        }
+
+        public static void TryConnect(IPAddress ip, int port, Queue<TimeSpan> leftTimeoutList, Action<bool, Socket, object> onCompleted)
+        {
+            if(ip == null)
+            {
+                throw new ArgumentNullException(nameof(ip));
+            }
+
+            var remoteEndPoint = new IPEndPoint(ip, port);
+
+            DoTryConnect(remoteEndPoint, leftTimeoutList, onCompleted);
+        }
+
+        private static void DoTryConnect(EndPoint remoteEndPoint, Queue<TimeSpan> leftTimeoutList, Action<bool, Socket, object> onCompleted)
+        {
+            if(remoteEndPoint == null)
+            {
+                throw new ArgumentNullException(nameof(remoteEndPoint));
+            }
+
+            var socket = new Socket(remoteEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            var args = new SocketAsyncEventArgs();
+            args.RemoteEndPoint = remoteEndPoint;
+            args.Completed += OnConnect;
+
+            try
+            {
+                bool willRaiseEventLater = socket.ConnectAsync(args);
+                if (!willRaiseEventLater)
+                {
+                    //연결이 바로 완료될 시
+                    ProcessConnect(args);
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"서버 연결 실패 - [{e.Message}]");
+                DisposeConnectionArgs(args);
+            }
+        }
+
+        private static void OnConnect(object sender, SocketAsyncEventArgs args)
+        {
+            ProcessConnect(args);
+        }
+
+        private static void ProcessConnect(SocketAsyncEventArgs args)
+        {
+
+        }
+
+        private static void DisposeConnectionArgs(SocketAsyncEventArgs args)
+        {
+            args.Dispose();
+        }
+
+        public void Connect()
+        {
+
+        }
     }
 }
