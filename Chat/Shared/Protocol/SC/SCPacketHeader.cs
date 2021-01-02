e616414ae1a42feb93bd1ad560ff478553b437c3
wlsvy <hubjpkim@gmail.com>
2021-01-02 18:03:35 +0900

    패킷 헤더 using 정리

--- Chat/Shared/Protocol/SC/SCPacketHeader.cs
+++ Chat/Shared/Protocol/SC/SCPacketHeader.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Shared.Protocol.SC
+﻿namespace Shared.Protocol.SC
 {
     public readonly struct SCPacketHeader
     {
