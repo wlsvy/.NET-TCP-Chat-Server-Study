ed77d1536c11b03ffb4cee3933e185c177615dde
wlsvy <hubjpkim@gmail.com>
2021-01-02 17:48:39 +0900

    Tcp 작업 진행 중

--- Chat/Server/Core/ClientConnection.cs
+++ Chat/Server/Core/ClientConnection.cs
@@ -1,7 +1,16 @@
-﻿namespace Server.Core
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Server.Core
 {
     public sealed class ClientConnection
     {
+        public readonly long Id;
 
+
+        public ClientConnection(long id, Socket tcpSocket, SessionManager sessionManager)
+        {
+
+        }
     }
 }
--- Chat/Server/Core/ClientConnectionManager.cs
+++ Chat/Server/Core/ClientConnectionManager.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace Server.Core
 {
@@ -9,21 +10,38 @@ namespace Server.Core
     {
         private readonly AsyncTCPAcceptor m_Acceptor;
         private readonly SessionManager m_SessionManager;
+        private readonly ConcurrentDictionary<long, ClientConnection> m_Connections;
+
+        private long m_PreviousClientConnectionId = -1;
+        private bool m_IsDisposed = false;
 
         public ClientConnectionManager(SessionManager sessionManager)
         {
             m_Acceptor = new AsyncTCPAcceptor(OnNewConnection);
             m_SessionManager = sessionManager;
+            m_Connections = new ConcurrentDictionary<long, ClientConnection>();
         }
 
         public void Dispose()
         {
+            if (m_IsDisposed)
+            {
+                return;
+            }
+            m_IsDisposed = true;
+
             m_Acceptor?.Dispose();
         }
 
         private void OnNewConnection(Socket socket)
         {
+            long nextConnectionId = GenerateClientConnectionId();
 
+        }
+
+        private long GenerateClientConnectionId()
+        {
+            return Interlocked.Increment(ref m_PreviousClientConnectionId);
         }
 
     }
