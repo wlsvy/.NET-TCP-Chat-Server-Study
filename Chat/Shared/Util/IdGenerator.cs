af8e8bab3058b7722a5e5fe9074b0d58998de915
wlsvy <hubjpkim@gmail.com>
2021-01-05 23:58:51 +0900

    IdGenerator 추가

---
+++ Chat/Shared/Util/IdGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+
+namespace Shared.Util
+{
+    public sealed class IdGenerator
+    {
+        private long m_LastAssignedId;
+
+        public IdGenerator(long startNumber = 0)
+        {
+            m_LastAssignedId = startNumber - 1;
+        }
+
+        public long Generate()
+        {
+            return Interlocked.Increment(ref m_LastAssignedId);
+        }
+    }
+}
