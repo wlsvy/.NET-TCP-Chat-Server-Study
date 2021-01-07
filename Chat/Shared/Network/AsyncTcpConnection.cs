27aa6e27b9dcdb4677c6a6f56fce8ae9fd459d2e
wlsvy <hubjpkim@gmail.com>
2021-01-08 01:13:39 +0900

    AsyncTcpConnection 작업

--- Chat/Shared/Network/AsyncTcpConnection.cs
+++ Chat/Shared/Network/AsyncTcpConnection.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using System.Linq;
 using Shared.Logger;
 
 namespace Shared.Network
@@ -35,14 +36,23 @@ namespace Shared.Network
             }
         }
 
+        
+
+        private const int INITIAL_READ_BYTES = 1024;
+        private const int READ_BYTES_INCREASING_RATE = 2;
+
         public readonly IPEndPoint RemoteEndPoint;
 
         public bool IsConnected => m_Socket.Connected;
         public bool IsClosed => (Volatile.Read(ref m_IsDisposed) != 0);
 
         private readonly Socket m_Socket;
-        private readonly Action<Exception> m_OnSendError;
         private readonly SocketAsyncEventArgs m_SocketAsyncEventArgs = new SocketAsyncEventArgs();
+        private readonly Action<Exception> m_OnSendError;
+
+        private Func<ArraySegment<byte>, int> m_OnReceived;
+        private Action<Exception> m_OnReceiveError;
+        private Action m_OnReceiveCompleted;
 
         private int m_IsDisposed = 0;
 
@@ -76,6 +86,156 @@ namespace Shared.Network
             m_Socket.Shutdown(SocketShutdown.Both);
             m_Socket.Close(timeout: 0);
             m_SocketAsyncEventArgs.Dispose();
+        }
+
+        public void Subscribe(Func<ArraySegment<byte>, int> onReceived, Action<Exception> onError, Action onCompleted)
+        {
+            m_OnReceived = onReceived ?? throw new ArgumentNullException(nameof(onReceived));
+            m_OnReceiveError = onError ?? throw new ArgumentNullException(nameof(onError));
+            m_OnReceiveCompleted = onCompleted ?? throw new ArgumentNullException(nameof(onCompleted));
+
+            if (IsClosed)
+            {
+                m_OnReceiveError.Invoke(new SocketException((int)SocketError.OperationAborted));
+            }
+            else
+            {
+                TryReceive();
+            }
+        }
+
+        private void TryReceive()
+        {
+            if (!m_Receiving.TryEnter())
+            {
+                return;
+            }
+
+            var receiveAsyncEventArgs = new SocketAsyncEventArgs
+            {
+                UserToken = new ReceiveContextData(2 * INITIAL_READ_BYTES)
+            };
+            receiveAsyncEventArgs.Completed += (sender, e) => OnReceiveCompleted(e);
+
+            try
+            {
+                receiveAsyncEventArgs.PrepareReceiveBuffer(INITIAL_READ_BYTES);
+                bool willRaiseEventLater;
+                while(!(willRaiseEventLater = ReceiveAsync(receiveAsyncEventArgs)))
+                {
+                    var canReceive = CompleteReceive(receiveAsyncEventArgs);
+                    if (!canReceive)
+                    {
+                        break;
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                CloseSocketWhileReceiving(receiveAsyncEventArgs, e);
+            }
+        }
+
+        private void OnReceiveCompleted(SocketAsyncEventArgs args)
+        {
+            try
+            {
+                bool willRaiseEventLater;
+                do
+                {
+                    var canReceive = CompleteReceive(args);
+                    if (!canReceive)
+                    {
+                        break;
+                    }
+                } while (!(willRaiseEventLater = ReceiveAsync(args)));
+            }
+            catch(Exception e)
+            {
+                CloseSocketWhileReceiving(args, e);
+            }
+        }
+
+        private bool ReceiveAsync(SocketAsyncEventArgs args)
+        {
+            if (IsClosed)
+            {
+                args.SocketError = SocketError.OperationAborted;
+                return false;
+            }
+            return m_Socket.ReceiveAsync(args);
+        }
+
+        private bool CompleteReceive(SocketAsyncEventArgs args)
+        {
+            if(args.SocketError != SocketError.Success)
+            {
+                CloseSocketWhileReceiving(args, new SocketException((int)args.SocketError));
+                return false;
+            }
+
+            var bytesReceiveRequested = args.Count;
+            if(args.BytesTransferred <= 0 && 0 < bytesReceiveRequested)
+            {
+                CloseSocketWhileReceiving(args);
+                return false;
+            }
+
+            var receivedEventData = args.UserToken as ReceiveContextData;
+            receivedEventData.ExpandReceiveBuffer(args.BytesTransferred);
+
+            var bytesConsumed = m_OnReceived(receivedEventData.ReceiveBuffer);
+            if(bytesConsumed > 0)
+            {
+                receivedEventData.SkipConsumedData(bytesConsumed);
+            }
+            else if(bytesConsumed < 0)
+            {
+                return false;
+            }
+
+            int bytesToReceive;
+            if(args.BytesTransferred < bytesReceiveRequested)
+            {
+                bytesToReceive = INITIAL_READ_BYTES;
+            }
+            else
+            {
+                var nextRequestReadBytes = READ_BYTES_INCREASING_RATE;
+                if(nextRequestReadBytes < 0)
+                {
+                    nextRequestReadBytes = int.MaxValue;
+                }
+                bytesToReceive = nextRequestReadBytes;
+            }
+
+            args.PrepareReceiveBuffer(bytesToReceive);
+            return true;
+        }
+
+        private void CloseSocketWhileReceiving(SocketAsyncEventArgs args, Exception exception)
+        {
+            m_Receiving.TryDispose();
+            if(args.UserToken is ReceiveContextData receiveCtx)
+            {
+                receiveCtx.Dispose();
+                args.UserToken = null;
+            }
+            args.Dispose();
+            m_OnReceiveError.Invoke(exception);
+            Dispose();
+        }
+        private void CloseSocketWhileReceiving(SocketAsyncEventArgs args)
+        {
+            m_Receiving.TryDispose();
+            if (args.UserToken is ReceiveContextData receiveCtx)
+            {
+                receiveCtx.Dispose();
+                args.UserToken = null;
+            }
+            args.Dispose();
+            m_OnReceiveCompleted.Invoke();
+            Dispose();
         }
 
         private void OnSendCompleted(SocketAsyncEventArgs args)
@@ -127,7 +287,6 @@ namespace Shared.Network
                 args.Dispose();
             }
             m_OnSendError?.Invoke(exception);
-            //Log.I.Warn($"{nameof(AsyncTcpConnection)}.{nameof(this.CloseSocketWhileSending)} 전송 중 오류 발생, Exception [{exception.Message}]");
             Dispose();
         }
     }
---
+++ Chat/Shared/Network/NetworkUtil.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+
+namespace Shared.Network
+{
+    internal static class NetworkUtil
+    {
+        internal static void PrepareReceiveBuffer(this SocketAsyncEventArgs receiveContext, int bytesToReceive)
+        {
+            var receiveContextData = receiveContext.UserToken as ReceiveContextData;
+
+            receiveContextData.CheckAndRearrangeReceiveBuffer(bytesToReceive);
+            var receiveBuffer = receiveContextData.ReceiveBuffer;
+            int beginning = receiveBuffer.Offset + receiveBuffer.Count;
+
+            if (bytesToReceive > (receiveBuffer.Array.Length - beginning))
+            {
+                throw new Exception("메모리 버퍼가 충분히 확보되지 않음");
+            }
+            receiveContext.SetBuffer(receiveBuffer.Array, beginning, bytesToReceive);
+        }
+    }
+
+    internal class ReceiveContextData : IDisposable
+    {
+        private ArraySegment<byte> m_ReceiveBuffer;
+
+        internal ArraySegment<byte> ReceiveBuffer => this.m_ReceiveBuffer;
+
+        internal ReceiveContextData(int initialBufferSize)
+        {
+            this.m_ReceiveBuffer = new ArraySegment<byte>(new byte[initialBufferSize], 0, 0);
+        }
+
+        public void Dispose()
+        {
+            this.m_ReceiveBuffer = default;
+        }
+
+        internal void ExpandReceiveBuffer(int bytesTransferred)
+        {
+            if (this.m_ReceiveBuffer.Count + bytesTransferred > this.m_ReceiveBuffer.Array.Length)
+            {
+                throw new ArgumentException(nameof(bytesTransferred));
+            }
+            this.m_ReceiveBuffer = new ArraySegment<byte>(this.m_ReceiveBuffer.Array,
+                                                     this.m_ReceiveBuffer.Offset,
+                                                     this.m_ReceiveBuffer.Count + bytesTransferred);
+        }
+
+        internal void SkipConsumedData(int bytesConsumed)
+        {
+            if (bytesConsumed > m_ReceiveBuffer.Count)
+            {
+                throw new ArgumentException(nameof(bytesConsumed));
+            }
+
+            m_ReceiveBuffer = new ArraySegment<byte>(m_ReceiveBuffer.Array, m_ReceiveBuffer.Offset + bytesConsumed, m_ReceiveBuffer.Count - bytesConsumed);
+        }
+
+        internal void CheckAndRearrangeReceiveBuffer(int bytesInNeed)
+        {
+            int capacity = this.m_ReceiveBuffer.Array.Length;
+            int actualDataBytes = this.m_ReceiveBuffer.Count;
+            int availableBeginning = this.m_ReceiveBuffer.Offset + actualDataBytes;
+            int availableBytes = capacity - availableBeginning;
+
+            if (availableBytes >= bytesInNeed)
+            {
+                return;
+            }
+
+            if (capacity - actualDataBytes >= bytesInNeed)
+            {
+                var originBuffer = this.m_ReceiveBuffer.Array;
+                Buffer.BlockCopy(originBuffer, this.m_ReceiveBuffer.Offset, originBuffer, 0, actualDataBytes);
+                this.m_ReceiveBuffer = new ArraySegment<byte>(originBuffer, 0, actualDataBytes);
+            }
+            else
+            {
+                int newCapacity = capacity * 2;
+                while ((newCapacity - actualDataBytes) < bytesInNeed)
+                {
+                    newCapacity += newCapacity;
+                }
+                var newBuffer = new byte[newCapacity];
+                var originBuffer = this.m_ReceiveBuffer.Array;
+                Buffer.BlockCopy(originBuffer, this.m_ReceiveBuffer.Offset, newBuffer, 0, actualDataBytes);
+                this.m_ReceiveBuffer = new ArraySegment<byte>(newBuffer, 0, actualDataBytes);
+            }
+
+            if (this.m_ReceiveBuffer.Array.Length - this.m_ReceiveBuffer.Offset + this.m_ReceiveBuffer.Count < bytesInNeed)
+            {
+                throw new Exception($"{nameof(ReceiveContextData)}.{nameof(CheckAndRearrangeReceiveBuffer)} 오류");
+            }
+        }
+    }
+}
