3d551896430e2512c72d75d1128c3861d639740d
wlsvy <hubjpkim@gmail.com>
2021-01-14 00:04:10 +0900

    Tcp 작업중

--- Chat/Shared/Network/AsyncTcpContexts.cs
+++ Chat/Shared/Network/AsyncTcpContexts.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Sockets;
 using System.Threading;
+using Shared.Util;
 
 namespace Shared.Network
 {
@@ -11,6 +11,7 @@ namespace Shared.Network
         private SpinLock m_LockSendingBufferQueue = new SpinLock(false);
 
         private bool m_IsDisposed = false;
+        private readonly TcpSendBufferQueue m_SendBufferQueue = new TcpSendBufferQueue();
 
         public void Dispose()
         {
@@ -19,12 +20,115 @@ namespace Shared.Network
             {
                 m_LockSendingBufferQueue.Enter(ref lockTaken);
                 m_IsDisposed = true;
+                m_SendBufferQueue.Clear();
+                m_SendBufferQueue.TrimExcess();
             }
             finally
             {
                 if (lockTaken)
                 {
                     m_LockSendingBufferQueue.Exit();
+                }
+            }
+        }
+
+        internal bool IsEmpty
+        {
+            get
+            {
+                var lockTaken = false;
+                try
+                {
+                    m_LockSendingBufferQueue.Enter(ref lockTaken);
+                    return m_SendBufferQueue.List.IsEmpty();
+                }
+                finally
+                {
+                    if (lockTaken)
+                    {
+                        m_LockSendingBufferQueue.Exit();
+                    }
+                }
+            }
+        }
+
+        internal void Add(ArraySegment<byte> sendBuffer)
+        {
+            var lockTaken = false;
+            try
+            {
+                m_LockSendingBufferQueue.Enter(ref lockTaken);
+                if (m_IsDisposed)
+                {
+                    return;
+                }
+                m_SendBufferQueue.Add(sendBuffer);
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    m_LockSendingBufferQueue.Exit();
+                }
+            }
+        }
+
+        internal void AddRange(IEnumerable<ArraySegment<byte>> sendBuffers)
+        {
+            var lockTaken = false;
+            try
+            {
+                m_LockSendingBufferQueue.Enter(ref lockTaken);
+                if (m_IsDisposed)
+                {
+                    return;
+                }
+                foreach(var sendBuffer in sendBuffers)
+                {
+                    m_SendBufferQueue.Add(sendBuffer);
+                }
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    m_LockSendingBufferQueue.Exit();
+                }
+            }
+        }
+
+        internal void CopyBufferListTo(SocketAsyncEventArgs args)
+        {
+            var lockTaken = false;
+            try
+            {
+                m_LockSendingBufferQueue.Enter(ref lockTaken);
+                args.BufferList = m_SendBufferQueue.List;
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    m_LockSendingBufferQueue.Exit(lockTaken);
+                }
+            }
+        }
+
+        internal bool Skip(int bytesSent)
+        {
+            var lockTaken = false;
+            try
+            {
+                m_LockSendingBufferQueue.Enter(ref lockTaken);
+                m_SendBufferQueue.TrimExcess();
+                m_SendBufferQueue.Remove(bytesSent);
+                return !m_SendBufferQueue.List.IsEmpty();
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    m_LockSendingBufferQueue.Exit(lockTaken);
                 }
             }
         }
--- Chat/Shared/Util/Extension.cs
+++ Chat/Shared/Util/Extension.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Shared.Util
 {
     public static class Extension
     {
-        public static bool Empty<T>(this ICollection<T> collection)
+        public static bool IsEmpty<T>(this ICollection<T> collection)
         {
             return collection.Count == 0;
         }
