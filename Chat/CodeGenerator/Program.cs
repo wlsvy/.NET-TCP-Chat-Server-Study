86eb21c7c0b061c5b37ed8e6510cb6738653bab2
wlsvy <hubjpkim@gmail.com>
2021-01-24 16:48:30 +0900

    코드젠 작업 중

--- Chat/CodeGenerator/Program.cs
+++ Chat/CodeGenerator/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace CodeGenerator
 {
@@ -6,7 +12,26 @@ namespace CodeGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var docs = LoadXml();
+
+        }
+
+        private static IReadOnlyCollection<(string, XDocument)> LoadXml()
+        {
+            var docs = new ConcurrentQueue<(string, XDocument)>();
+
+            var tasks = new List<Task>();
+            foreach (var path in Directory.GetFiles("Schema", "*.xml", SearchOption.AllDirectories))
+            {
+                tasks.Add(Task.Run(() =>
+                {
+                    var doc = XDocument.Load(path, LoadOptions.SetLineInfo);
+                    var item = (path, doc);
+                    docs.Enqueue(item);
+                }));
+            }
+            Task.WaitAll(tasks.ToArray());
+            return docs.ToArray();
         }
     }
 }
