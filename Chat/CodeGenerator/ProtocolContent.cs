bb900fb3232805014e7512187796d1905ab62795
wlsvy <hubjpkim@gmail.com>
2021-01-27 00:08:52 +0900

    코드젠 작업 중

---
+++ Chat/CodeGenerator/ProtocolContent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CodeGenerator
+{
+    internal readonly struct ProtocolContent
+    {
+        public readonly string ProtocolName;
+        public readonly (Type paramType, string paramName)[] Parameters;
+
+        public ProtocolContent(string name, (Type paramType, string paramName)[] parameters)
+        {
+            ProtocolName = name;
+            Parameters = parameters;
+        }
+    }
+}
--- Chat/CodeGenerator/XmlParser.cs
+++ Chat/CodeGenerator/XmlParser.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
 using System.Xml.Linq;
 
 namespace CodeGenerator
@@ -16,12 +16,29 @@ namespace CodeGenerator
 
         public static void ToNetworkMessage(string path, XDocument doc)
         {
-           foreach(var element in doc.Elements())
+            foreach (var element in doc.Elements())
             {
                 switch (element.Name.LocalName)
                 {
-                    case "ServerToClient": break;
-                    case "ClientToServer": break;
+                    case "ServerToClient": ParseProtocol(path, element); break;
+                    case "ClientToServer": ParseProtocol(path, element); break;
+                    default: throw new NotImplementedException();
+                }
+            }
+        }
+
+        private static void ParseProtocol(string path, XElement protocols)
+        {
+            //var content = new ProtocolContent
+            var elements = protocols.Elements();
+            foreach (var protocol in protocols.Elements())
+            {
+                var paramList = new List<(Type paramType, string paramName)>();
+                foreach(var param in protocol.Elements())
+                {
+                    var typeName = param.Name.NamespaceName;
+                    var paramName = param.Name.LocalName;
+                    paramList.Add((Type.GetType(param.Name.NamespaceName), param.Name.LocalName));
                 }
             }
         }
