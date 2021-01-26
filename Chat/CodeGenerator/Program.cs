389d026ce62799e4ffe0c89ce60420df5cca50f1
wlsvy <hubjpkim@gmail.com>
2021-01-26 22:37:52 +0900

    코드젠 작업 중

--- Chat/CodeGenerator/Program.cs
+++ Chat/CodeGenerator/Program.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Concurrent;
+﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
-using System.Xml;
 using System.Xml.Linq;
 
 namespace CodeGenerator
@@ -13,21 +11,21 @@ namespace CodeGenerator
         static void Main(string[] args)
         {
             var docs = LoadXml();
-
+            
         }
 
         private static IReadOnlyCollection<(string, XDocument)> LoadXml()
         {
             var docs = new ConcurrentQueue<(string, XDocument)>();
+            var paths = Directory.GetFiles("Schema", "*.xml", SearchOption.AllDirectories);
+            var tasks = new List<Task>(paths.Length);
 
-            var tasks = new List<Task>();
-            foreach (var path in Directory.GetFiles("Schema", "*.xml", SearchOption.AllDirectories))
+            foreach (var path in paths)
             {
                 tasks.Add(Task.Run(() =>
                 {
                     var doc = XDocument.Load(path, LoadOptions.SetLineInfo);
-                    var item = (path, doc);
-                    docs.Enqueue(item);
+                    docs.Enqueue((path, doc));
                 }));
             }
             Task.WaitAll(tasks.ToArray());
---
+++ Chat/CodeGenerator/XmlParser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace CodeGenerator
+{
+    internal static class XmlParser
+    {
+        public static void ToNetworkMessage(IReadOnlyCollection<(string path, XDocument doc)> list)
+        {
+            foreach(var (path, doc) in list)
+            {
+                ToNetworkMessage(path, doc);
+            }
+        }
+
+        public static void ToNetworkMessage(string path, XDocument doc)
+        {
+           foreach(var element in doc.Elements())
+            {
+                switch (element.Name.LocalName)
+                {
+                    case "ServerToClient": break;
+                    case "ClientToServer": break;
+                }
+            }
+        }
+    }
+}
