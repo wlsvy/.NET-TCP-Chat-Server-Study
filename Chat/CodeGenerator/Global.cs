634689fc4001bf1fd53bb3688b1b1cb9913b4475
wlsvy <hubjpkim@gmail.com>
2021-01-28 23:01:04 +0900

    코드젠 작업중

---
+++ Chat/CodeGenerator/Global.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGenerator
+{
+    internal static class Global
+    {
+        public enum Directories : byte
+        {
+            Root,
+            Shared_Protocol
+        }
+
+        public const string ROOT_DIRECTORY_NAME = "Chat";
+
+        private static DirectoryInfo s_RootDirectoryPath;
+        public static string ROOT_DIRECTORY_PATH => s_RootDirectoryPath.FullName;
+
+        public static Dictionary<Directories, string> s_DirectoryDic;
+        public static IReadOnlyDictionary<Directories, string> DIRECTORY_DIC => s_DirectoryDic;
+
+        public static void Initialize()
+        {
+            s_RootDirectoryPath = FindRootDirectory() ?? throw new DirectoryNotFoundException("루트 디렉토리를 찾지 못했습니다");
+            s_DirectoryDic = new Dictionary<Directories, string>() 
+            {
+                { Directories.Root, ROOT_DIRECTORY_PATH },
+                { Directories.Shared_Protocol, Path.Combine(ROOT_DIRECTORY_PATH, "Shared", "Protocol") },
+            };
+        }
+
+        private static DirectoryInfo FindRootDirectory()
+        {
+            var directory = Directory.GetParent(Directory.GetCurrentDirectory());
+            while (directory != null &&
+                directory.Name != ROOT_DIRECTORY_NAME)
+            {
+                directory = directory.Parent;
+            }
+
+            if (directory == null ||
+                directory.Name != ROOT_DIRECTORY_NAME)
+            {
+                return null;
+            }
+
+            return directory;
+        }
+    }
+}
