643c79afd87c8a5893b063c07670eaa718f264d4
wlsvy <hubjpkim@gmail.com>
2021-02-04 22:12:47 +0900

    서버 데이터 소스 작업 중.... (DB연동 해야하나...)

--- Chat/Server/Network/CSPacketHandler.cs
+++ Chat/Server/Network/CSPacketHandler.cs
@@ -1,10 +1,20 @@
 ﻿using Shared.Logger;
 using Shared.Protocol;
+using System;
 
 namespace Server.Network
 {
     public sealed class CSPacketHandler : ICSPacketHandler
     {
+        private readonly ClientConnection m_Connection;
+        private readonly SessionManager m_SessionManager;
+
+        public CSPacketHandler(ClientConnection connection, SessionManager sessionManager)
+        {
+            m_Connection = connection ?? throw new ArgumentNullException(nameof(connection));
+            m_SessionManager = sessionManager ?? throw new ArgumentNullException(nameof(connection));
+        }
+
         public void HANDLE_CS_Ping(long sequenceNumber)
         {
             Log.I.Info($"Network Ping : {sequenceNumber}");
